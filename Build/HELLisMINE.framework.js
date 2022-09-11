30aa887fb530cf3aeeb3b9e15e872dc1506f8682
hantabaru1014 <hantabaru1014@gmail.com>
2022-09-11 17:59:18 +0900

    Build on Commit: 3ce4514a81c2856965ea2a3ccfec25206b479ad1

--- Build/HELLisMINE.framework.js
+++ Build/HELLisMINE.framework.js
@@ -1292,7 +1292,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2920576;
+STATICTOP = STATIC_BASE + 2921488;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3366,7 +3366,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2920576;
+var STATIC_BUMP = 2921488;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
